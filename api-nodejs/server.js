49aa6a82e093ec23aee071a989a224d96bdf49c1
up1 <somkiat.p@gmail.com>
2024-05-15 16:51:44 +0700

    config dynamic port from env

--- api-nodejs/server.js
+++ api-nodejs/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const os = require("os");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Generate openapi
 const expressOasGenerator = require("express-oas-generator");
